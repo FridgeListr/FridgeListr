c4228c40778fc6f201ba6bf0969cd4ad7f5b3bc6
FrozenStove <Yessysutter@gmail.com>
2022-08-12 20:39:19 -0700

    Co-authored-by: Henry Park <codedenma@gmail.com> Co-authored-by: Carlos Villarreal <lquilpensl@gmail.com> Co-authored-by: Si Young Mah <siyoungmah@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+
+const PORT = process.env.PORT || 3000;
+
+// handle requests for static files for css styling, index.html and bundle.js
+// looks for static files, specifically index.html, style.css, index.js
+console.log(path.join(__dirname, "../client"));
+app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(path.resolve(__dirname, "../bundle")))
+
+
+app.listen(PORT, () => {    
+    console.log(`Server listening on port: ${PORT}...`);
+});
