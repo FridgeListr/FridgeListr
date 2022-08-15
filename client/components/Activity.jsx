a9ef98744e19f4dcc810fec99d2676d0e6c909a1
FrozenStove <Yessysutter@gmail.com>
2022-08-15 11:21:58 -0700

    Progress

--- client/components/Activity.jsx
+++ client/components/Activity.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 // import 
 
 // create a container to hold queried data
+const activityList = (props) => {
+
+}
 
 /* Select data from DB with food expiring within 3 days
 will need to get current date, and compare with all dates from DB */
